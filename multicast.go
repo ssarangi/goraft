fc6fafad433b34ce8f6b55d30472924891efa937
Satyajit Sarangi <satyajits@google.com>
2018-05-30 21:56:32 -0700

    Initial commit for Raft protocol implementation

---
+++ multicast.go
@@ -0,0 +1,54 @@
+package multicast
+
+import (
+	"log"
+	"net"
+)
+
+const (
+	maxDatagramSize = 8192
+)
+
+// NewBroadcaster creates a new UDP multicast connection on which to broadcast
+func NewBroadcaster(address string) (*net.UDPConn, error) {
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+// Listen binds to the UDP address and port given and writes packets received
+// from that address to a buffer which is passed to a handler
+func Listen(address string, handler func(*net.UDPAddr, int, []byte)) {
+	// Parse the string address
+	addr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open up a connection
+	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn.SetReadBuffer(maxDatagramSize)
+
+	// Loop forever reading from the socket
+	for {
+		buffer := make([]byte, maxDatagramSize)
+		numBytes, src, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed: ", err)
+		}
+
+		handler(src, numBytes, buffer)
+	}
+}
